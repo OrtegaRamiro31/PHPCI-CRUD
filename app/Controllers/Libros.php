b22cb46757304a09c1477f6921b3550ca0b20a61
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2022-07-02 14:39:27 -0500

    first commit

---
+++ app/Controllers/Libros.php
@@ -0,0 +1,128 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\Libro;
+class Libros extends Controller{
+
+    public function index(){
+
+        $libro = new Libro();
+        $datos['libros'] = $libro -> orderBy('id','ASC')->findAll();
+
+        $datos['cabecera'] = view('template/cabecera');
+        $datos['pie'] = view('template/piepagina');
+
+
+        return view('libros/listar',$datos);
+
+    }
+    public function crear(){
+        $datos['cabecera'] = view('template/cabecera');
+        $datos['pie'] = view('template/piepagina');
+
+        return view('libros/crear', $datos);
+    }
+
+    public function guardar(){
+        
+        $libro = new Libro();
+
+        $validacion = $this->validate([
+            'nombre'=>'required|min_length[3]',
+            'imagen' =>[
+                'uploaded[imagen]',
+                'mime_in[imagen,image/jpg,image/jpeg,image/png]',
+                'max_size[imagen,1024]',
+                ]
+        ]);
+
+        if(!$validacion){
+            $session = session();
+            $session->setFlashdata('mensaje','Imagen mayor a 1024px o nombre menor a 3 caracteres.');
+
+            return redirect()->back()->withInput();
+            //return $this->response->redirect(site_url('/listar'));
+        }
+
+        if($imagen=$this->request->getFile('imagen')){
+            $nuevoNombre = $imagen->getRandomName();
+            $imagen->move('../public/uploads/',$nuevoNombre);
+            $datos=[
+                'nombre'=> $this->request->getVar('nombre'),
+                'imagen'=>$nuevoNombre
+            ];
+            
+            $libro->insert($datos);
+        }
+        
+        return $this->response->redirect(site_url('/listar'));
+    }
+    
+    public function borrar($id=null){
+        $libro = new Libro();
+        $datosLibro = $libro->where('id',$id)->first();
+        
+        $ruta = ('../public/uploads/'.$datosLibro['imagen']);
+        unlink($ruta);
+
+        $libro->where('id',$id)->delete($id);
+
+        return $this->response->redirect(site_url('/listar'));
+    }
+    public function editar($id = null){
+        print_r($id);
+        $libro = new Libro();
+        $datos['libro']=$libro->where('id',$id)->first();
+
+        $datos['cabecera'] = view('template/cabecera');
+        $datos['pie'] = view('template/piepagina');
+
+        return view('libros/editar', $datos);
+    }
+    public function actualizar(){
+        $libro = new Libro();
+        $datos=[
+            'nombre'=> $this->request->getVar('nombre')
+            ];
+            $id = $this->request->getVar('id');
+
+            
+            $validacion = $this->validate([
+                'nombre'=>'required|min_length[3]'
+            ]);
+    
+            if(!$validacion){
+                $session = session();
+                $session->setFlashdata('mensaje','Revise la información.');
+    
+                return redirect()->back()->withInput();
+                //return $this->response->redirect(site_url('/listar'));
+            }
+
+
+            $libro->update($id,$datos);
+
+            $validacion = $this->validate([
+                'imagen' =>[
+                    'uploaded[imagen]',
+                    'mime_in[imagen,image/jpg,image/jpeg,image/png]',
+                    'max_size[imagen,1024]',
+                    ]
+                ]);
+            if($validacion){
+                if($imagen=$this->request->getFile('imagen')){
+                    $datosLibro=$libro->where('id',$id)->first();
+                    
+                    $ruta = ('../public/uploads/'.$datosLibro['imagen']);
+                    unlink($ruta);
+
+                    $nuevoNombre = $imagen->getRandomName();
+                    $imagen->move('../public/uploads/',$nuevoNombre);
+                    $datos=['imagen'=>$nuevoNombre];
+                    $libro->update($id,$datos);
+                }
+            }
+        return $this->response->redirect(site_url('/listar'));
+    }
+}
---
+++ app/Views/template/cabecera.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Ramiro</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand">CRUD EN LA NUBE</a>
+        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="my-nav" class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?=base_url('listar')?>">Inicio</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        
+    <?php if(session('mensaje')){?>
+<div class="alert alert-danger" role="alert">
+    <?php
+        echo session('mensaje');
+    ?>
+</div>
+<?php
+}
+?>
